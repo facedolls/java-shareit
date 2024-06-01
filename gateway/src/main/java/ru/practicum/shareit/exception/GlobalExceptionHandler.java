9f5f7a2b03fb3435cdbea2e23238f1a38e3c143c
Dennis T <facedolls@gmail.com>
2024-06-01 20:58:56 +0300

    refactor: move validation test to gateway

--- gateway/src/main/java/ru/practicum/shareit/exception/GlobalExceptionHandler.java
+++ gateway/src/main/java/ru/practicum/shareit/exception/GlobalExceptionHandler.java
@@ -8,27 +8,12 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import javax.validation.ConstraintViolation;
 import javax.validation.ConstraintViolationException;
-import javax.validation.ValidationException;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @RestControllerAdvice("ru.practicum")
 @Slf4j
 public class GlobalExceptionHandler {
-    @ExceptionHandler
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public GlobalErrorResponse handleNotFoundException(final NotFoundException e) {
-        log.warn("404 {}", e.getMessage(), e);
-        return new GlobalErrorResponse(e.getMessage());
-    }
-
-    @ExceptionHandler
-    @ResponseStatus(HttpStatus.CONFLICT)
-    public GlobalErrorResponse handleConflictException(final ConflictException e) {
-        log.warn("409 {}", e.getMessage(), e);
-        return new GlobalErrorResponse(e.getMessage());
-    }
-
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public GlobalErrorResponse handleConstraintViolationException(final ConstraintViolationException e) {
@@ -39,12 +24,5 @@ public class GlobalExceptionHandler {
         log.warn("400 {}", errors);
         String errorMessage = String.join(", ", errors);
         return new GlobalErrorResponse(errorMessage);
-    }
-
-    @ExceptionHandler
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public GlobalErrorResponse handleValidationException(final ValidationException e) {
-        log.warn("400 {}", e.getMessage(), e);
-        return new GlobalErrorResponse(e.getMessage());
     }
 }
